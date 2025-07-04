ac0ffbe56e2518d706622c330521d5f71d081437
Connor Mills <connor@virtualized.dev>
2025-07-03 18:31:29 -0700

    This does not need to update

--- src/main/constants.ts
+++ src/main/constants.ts
@@ -2,12 +2,8 @@ import path from 'node:path'
 import os from 'node:os'
 import { settingsService } from './services/settings.service'
 
-function getModsDir(): Record<string, string> {
-  return {
-    win32: path.join(os.homedir(), 'AppData', 'Roaming', 'Balatro', 'Mods'),
-    darwin: path.join(os.homedir(), 'Library', 'Application Support', 'Balatro', 'Mods'),
-    linux: settingsService.getLinuxModsDirectory()
-  }
+export const MODS_DIR = {
+  win32: path.join(os.homedir(), 'AppData', 'Roaming', 'Balatro', 'Mods'),
+  darwin: path.join(os.homedir(), 'Library', 'Application Support', 'Balatro', 'Mods'),
+  linux: settingsService.getLinuxModsDirectory()
 }
-
-export const MODS_DIR = getModsDir()
